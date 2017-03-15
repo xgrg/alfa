920fcd29fb2e34d0646e34e66863afa6952fa65e
Grégory Operto <goperto@gmail.com>
2017-03-15 13:38:22 +0100

    Added roicollect.py (and updated notebooks accordingly)

--- roicollect.py
+++ roicollect.py
@@ -4,11 +4,14 @@ import pandas as pd
 from glob import glob
 import os.path as osp
 import numpy as np
+import seaborn
+from matplotlib.font_manager import FontProperties
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 def getdict(data, key, column, column2=None, value=None):
     ''' From a Pandas DataFrame returns a dictionary given a key and a column,
     with the possibility of filtering on the value of a second column'''
-    import string
     if not value is None:
         data = data[data[column2] == value]
 
@@ -17,26 +20,23 @@ def getdict(data, key, column, column2=None, value=None):
 
     return d1
 
-def collect_roivalues(roilabel, src=None, verbose=False):
+def collect_roivalues(roilabel, csvfiles, subjects, verbose=False):
     ''' This simply collects the values from *_stats.csv files and returns a
     DataFrame with these ROI values, given a specific `roilabel`.
     `src` is the folder containing the csv files.
     '''
-    import string
-    if src is None:
-        src = '/tmp/roivalues_csf.5/' # new csv with the artifactual roi (temporal)
-        print 'Warning: default csv files loaded from ', src
-
-    subjects = [string.atoi(osp.split(e)[-1].split('_')[0])\
-            for e in glob(osp.join(src, '*.csv'))]
-    print len(subjects), 'subjects displayed'
-
     data = []
     subj = []
-    for s in subjects:
-        fp = osp.join(src, '%s_stats.csv'%s)
-        if verbose:
-            print s, fp
+
+    assert(len(csvfiles) == len(subjects))
+    for s, fp in zip(subjects, csvfiles):
+        #fp = osp.join(src, filepattern%s)
+        #if verbose:
+        #    print s, fp
+        #if not osp.isfile(fp):
+        #    if len(glob(fp)) != 1:
+        #        raise AssertionError('multiple files found %s'%str(glob(fp)))
+        #    fp = glob(fp)[0]  #in case a glob string is passed
         df = pd.read_csv(fp, sep='\t').set_index('ROI_label')
         try:
             data.append(df.ix[roilabel]['mean'])
@@ -48,9 +48,9 @@ def collect_roivalues(roilabel, src=None, verbose=False):
     return pd.DataFrame(data, index=subj, columns=['roi'])
 
 def correct(df):
-    import statsmodels.api as sm
-    from statsmodels.formula.api import ols
+
     model = 'roi ~ 1 + C(apo) + gender + educyears + ventricles'
+    print 'Model used for correction:', model
     test_scores = ols(model, data=df).fit()
 
     err = test_scores.predict(df) - df['roi']
@@ -58,98 +58,138 @@ def correct(df):
 
     return ycorr
 
-def set_figaxes(df):
-    plt.xlabel('age')
-    plt.ylabel('roi')
-    plt.ylim([0.0007, 0.0010]) #df['roi'].max()])
+def set_figaxes(df, ylim=None):
+    font = FontProperties(family='sans-serif', weight='heavy')
+    plt.xlabel('age', fontproperties=font)
+    plt.ylabel('roi', fontproperties=font)
+    if not ylim is None:
+        plt.ylim(ylim)
+    #else:
+     #df['roi'].max()])
     plt.xlim([df['age'].min(), df['age'].max()])
 
 
 def get_groups(dataset, groups_names=[]):
     # take each group separately
     if len(groups_names) == 0:
-        groups_names = ['apoe23', 'apoe24', 'apoe33', 'apoe34', 'apoe44']
+        groups_names = ['apoe44', 'apoe34', 'apoe24', 'apoe33', 'apoe23']
     groups1 = []
     for i in xrange(5):
         groups1.append(dataset[dataset['apo'] == i])
     groups = []
+    groups_ht = {'C': [1,3,4],
+                 'NC': [0,2],
+                 'HO': [4],
+                 'HT': [1,3],
+                 'All':[0,1,2,3]}
     for name in groups_names:
-        if name == 'C':
-            groups.append(pd.concat([groups1[i] for i in [1,3,4]]))
-        elif name == 'NC':
-            groups.append(pd.concat([groups1[i] for i in [0,2]]))
-        elif name == 'HO':
-            groups.append(pd.concat([groups1[i] for i in [4]]))
-        elif name == 'HT':
-            groups.append(pd.concat([groups1[i] for i in [1,3]]))
-        elif name == 'All':
-            groups.append(pd.concat([groups1[i] for i in [0,1,2,3]]))
+        for k,v in groups_ht.items():
+            if name == k:
+                groups.append(pd.concat([groups1[i] for i in v]))
     if len(groups) == 0:
         return groups1, groups_names
     return groups, groups_names
 
 
-def plot_region(roiname, dataset, order=1, ax=None):
-    import statsmodels.api as sm
-    from statsmodels.formula.api import ols
+def plot_region(dataset, roi_name, groups, order=1,
+        ax=None, ylim=[0.0005, 0.0010]):
+
     if ax == None:
         fig = plt.figure(figsize=(6, 6))
-        set_figaxes(dataset)
+        set_figaxes(dataset, ylim=ylim)
         ax = fig.add_subplot(111)
 
-    colors = ['#800000','#003366','g','m','y']
-    facecolors = ['#ff9999','#99ccff','g','m','y']
+    font = FontProperties(family='sans-serif', weight='light')
+
+    #TODO: dictionaries for always matched pairs groups/colors
+    edgecolors = ['#800000','#003366','#ff8000','#cc6699','#33cc33']
+    facecolors = ['#ff9999','#99ccff','#ffd699','#ecc6d9','#adebad']
+
     formulas = ['roi ~ 1 + age',
                 'roi ~ 1 + age + I(age**2)',
                 'roi ~ 1 + age + I(age**2) + I(age**3)']
-    print 'Region:', roiname, '- Fitting order:', order, '- Formula:', formulas[order-1]
 
-    groups_names = ['HO', 'All']
-    groups, groups_names = get_groups(dataset, groups_names = groups_names)
+    print 'Region:', roi_name, '- Fitting order:', order, '- Formula:', formulas[order-1]
 
+    # Splits the dataset into genotypic groups
+    groups_sub, groups = get_groups(dataset, groups_names = groups)
 
-    for i, df in enumerate(groups):
-        ax.scatter(df['age'], df['roi'], edgecolors=colors[i],
+    for i, df in enumerate(groups_sub):
+        # Plots the group cloud
+        ax.scatter(df['age'], df['roi'], edgecolors=edgecolors[i],
                    facecolors=facecolors[i], linewidth=0.5,
-                   label='%s'%groups_names[i].capitalize(), s=20, alpha=0.7)
-        x = pd.DataFrame({'age': np.linspace(df['age'].min(), df['age'].max(), 100)})
+                   label='%s'%groups[i].capitalize(), s=20, alpha=0.7)
 
+        # Fits a line on the group data
+        x = pd.DataFrame({'age': np.linspace(df['age'].min(), df['age'].max(), 100)})
         poly = ols(formula=formulas[order-1], data=df).fit()
         ypred = poly.predict(x)
-        #print np.std(ypred)
-        ax.plot(x['age'], ypred, color=colors[i], linestyle='-',
+
+        # Draws the fitted line
+        ax.plot(x['age'], ypred, color=edgecolors[i], linestyle='-',
                 label='%s n=%s $R^2$=%.2f $AIC$=%.2f $\sigma$=%.3e'
-                 % (groups_names[i].capitalize(), order, poly.rsquared, poly.aic, np.std(ypred)),
+                 % (groups[i].capitalize(), order, poly.rsquared, poly.aic, np.std(ypred)),
                 alpha=1.0)
 
+    # Legend, text, title...
     ax.legend(prop={'size':8})
-    ax.text(0.15, 0.95, roiname, horizontalalignment='center',
-        verticalalignment='center', transform = ax.transAxes)
-    plt.title(roiname)
+    ax.text(0.15, 0.95, roi_name, horizontalalignment='center',
+        verticalalignment='center', transform = ax.transAxes,
+        fontproperties=font)
+    plt.title(roi_name, fontproperties=font)
 
 
-def plot_regions(data, regions, labels=None, nb_orders=3):
-    nb_regions = len(regions)
-    fig = plt.figure(figsize=(8*nb_orders, 8*nb_regions), dpi=300, facecolor='white')
+def plot_regions(data, labels, csvfiles, subjects, names=None, groups=None,
+        nb_orders=1, do_correct=True, ylim=[0.0005, 0.0010]):
+    ''' Generates a plot of ROI values to be looked up in a set of csvfiles,
+    given a set of `labels`, a set of `subjects`.
+    `names` is a dictionary naming the ROIs
+    `groups` refers to the way the data should be grouped and displayed
+    (according to genotypes)
+    `nb_orders` (default: 1) sets how many orders/models to be fitted to the data
+    `do_correct` (default: True) applies correction for covariates if provided
+    in the table `data` (i.e. genotype, gender, education, ventricular volume)
+    `ylim` sets y-limits on the final plot. '''
 
-    if labels is None:
-        print 'WARNING: using default value for labels'
-        labels = ['left_occip', 'left_temporal', 'left_temporal2',
-            'right_perihorn', 'left_occip2', 'left_perihorn', 'left_wm',
-            'right_occip', 'right_temporal', 'left_insula']
+    fig = plt.figure(figsize=(8*nb_orders, 8*len(labels)), dpi=300, facecolor='white')
 
-    for i, roilabel in enumerate(regions):
-        roiname = '%s'%labels[roilabel-1]
-        # 1-st order
-        ax = fig.add_subplot(nb_regions, nb_orders, nb_orders*i + 1)
-        roivalues = collect_roivalues(roilabel)
-        df = data.join(roivalues)
-        print np.std(df['roi'])
-        ycorr = pd.DataFrame(correct(df), columns=['roi'])
-        df = data.join(ycorr)
-        df['subject'] = df.index
-        df = df.sort_values(['apo', 'subject']).dropna()
+    # Managing options
+    if names is None:
+        print 'WARNING: using default values for labels'
+        names = {1:'left_occip', 2:'left_temporal', 3:'left_temporal2',
+            4:'right_perihorn', 5:'left_occip2', 6:'left_perihorn', 7:'left_wm',
+            8:'right_occip', 9:'right_temporal', 10:'left_insula'}
+        print names
+    if groups is None:
+        groups = ['HO', 'HT', 'NC']
+        print 'using default groups'
 
-        set_figaxes(df)
-        print np.std(df['roi'])
-        plot_region(roiname, df, order=1, ax=ax)
+    # Iterate on labels
+    for i, roi_label in enumerate(labels):
+        roi_name = '%s'%names[roi_label]
+
+        # Iterate on orders
+        for order in range(1, nb_orders+1):
+
+            # 1-st order
+            ax = fig.add_subplot(len(labels), nb_orders, nb_orders*i + order)
+
+            # Fetch values for the given ROI and correct them for covariates
+            roivalues = collect_roivalues(roi_label, csvfiles=csvfiles, subjects=subjects)
+            df = data.join(roivalues)
+            print 'Standard deviation of label %s:'%roi_name,\
+                        np.std(df['roi'])
+            if do_correct:
+                ycorr = pd.DataFrame(correct(df), columns=['roi'])
+                df = data.join(ycorr)
+                df['subject'] = df.index
+                df = df.sort_values(['apo', 'subject']).dropna()
+                print 'Standard deviation after correction for covariates:',\
+                        np.std(df['roi'])
+            else:
+                df['subject'] = df.index
+                df = df.sort_values(['apo', 'subject']).dropna()
+
+            # Plots the corrected values and fits a line over them
+            set_figaxes(df, ylim=ylim) # Adjusts the axes according to the values
+            plot_region(df, roi_name, order=order, groups=groups, ax=ax)
