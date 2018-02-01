53834c5605357fa73c18bbbfffc36d48e11bd8e6
Grégory Operto <goperto@gmail.com>
2018-02-01 10:23:18 +0100

    cleaned remove_csf

--- remove_csf.py
+++ remove_csf.py
@@ -1,27 +1,42 @@
-import sys
-import nipype.interfaces.spm as spm
-import nipype.pipeline.engine as pe
+#! /usr/bin/env python
 import os, json
 from glob import glob
 import os.path as osp
 import numpy as np
 import nibabel as nib
 from nilearn import image
+import argparse
+import logging as log
 
-subjects = json.load(open('/home/grg/spm/data/subjects_dartel.json'))
 
+if __name__ == '__main__':
+    log.basicConfig(level=log.INFO)
+    parser = argparse.ArgumentParser(description='`remove_csf.py` looks for '\
+        'voxels with a CSF-probability higher than a given threshold and gives '\
+        'them 0 in a given image (removes any voxel from skull and scalp). '\
+        'Filenames of segmentation maps will be guessed '\
+        'from the grey matter mask file.'\
+        'Please make sure both are coregistered.')
+    parser.add_argument('image', help='image to remove voxels from')
+    parser.add_argument('grey_matter_mask', help='grey matter mask')
+    parser.add_argument('destdir', help='destination folder')
+    parser.add_argument('--threshold', type=float, help='threshold of CSF-probability',
+            default=0.0)
+    opts = parser.parse_args()
 
-for s in subjects:
-  try:
-    print s
-    mdfp = [e for e in glob('/home/grg/dartel/%s*_t1space.nii*'%s) if not '.minf' in e][0]
+    mdfp = opts.image #glob('/home/grg/dartel_final/native/%s*_t1space.nii'%s)[0]
     md = np.asarray(nib.load(mdfp).dataobj)
-    for i in [3,4,5]:
-       csffp = glob('/home/grg/data/ALFA_DWI/%s*/T1/%s*c%s*.nii'%(s,s,i))[0]
-       print csffp
+    log.info('Image has dimensions: %s'%str(md.shape))
+    for i, n in zip([3,4,5], ['CSF', 'Skull', 'Scalp']):
+       csffp = opts.grey_matter_mask.replace('c1', 'c%s'%i) #glob('/home/grg/data/AmylStaging/%s/T1/%s*spm_c%s.nii'%(s,s,i))[0]
+       if not osp.isfile(csffp):
+           raise IOError('%s not found'%csffp)
        csf = np.asarray(nib.load(csffp).dataobj)
-       md[csf>0.5] = 0
-    image.new_img_like(mdfp, md).to_filename('/tmp/dartel5/%s_MD_t1space_wo_csf.nii'%s)
-  except IndexError:
-    print s, 'error'
-
+       log.info('%s map filename was found at: %s (dimensions: %s)'
+               %(n, csffp, str(csf.shape)))
+       threshold = opts.threshold if i==3 else 0.0
+       md[csf > threshold] = 0
+    fp = osp.join(opts.destdir,
+            '%s_wo_csf.nii'%osp.basename(mdfp).split('.nii')[0])
+    image.new_img_like(mdfp, md).to_filename(fp)
+    log.info('Succesfully written %s'%fp)
