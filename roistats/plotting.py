abec1599c61b95c99fbff9bc8a6c32c1a9dc4586
Grégory Operto <goperto@gmail.com>
2018-02-08 12:25:23 +0100

    roistats

--- roistats/plotting.py
+++ roistats/plotting.py
@@ -1,25 +1,31 @@
-import seaborn
+import seaborn as sns
+import scipy as sc
+import pandas as pd
 import matplotlib as mpl
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import logging as log
+
 mpl.rcParams['figure.facecolor'] = 'white'
 
-def correct(df, model='roi ~ 1 + gender + age', verbose=False): #educyears + apo' ):
-    ''' Applies a correction for covariates to a given DataFrame'''
-    if verbose:
-        print 'Model used for correction:', model
-    test_scores = ols(model, data=df).fit()
-
-    err = test_scores.predict(df) - df['roi']
-    ycorr = np.mean(df['roi']) - err
+def correct(df, model='roi ~ 1 + gender + age'): #educyears + apo' ):
+    ''' Adjusts a variable according to a given model'''
+    model = ols(model, data=df)
+    depvar = model.endog_names
+    test_scores = model.fit()
+    err = test_scores.predict(df) - df[depvar]
+    ycorr = np.mean(df[depvar]) - err
 
     return ycorr
 
-def set_figaxes(df, ylim=None, xlim=None):
+def set_figaxes(df, x='age', y='roi', ylim=None, xlim=None):
     font = FontProperties(family='sans-serif', weight='heavy')
-    plt.xlabel('age', fontproperties=font)
-    plt.ylabel('roi', fontproperties=font)
+    plt.xlabel(x, fontproperties=font)
+    plt.ylabel(y, fontproperties=font)
     if not ylim is None:
         plt.ylim(ylim)
-    plt.xlim([df['age'].min()-1, df['age'].max()+1])
+    plt.xlim([df[x].min()-1, df[x].max()+1])
 
 
 def plot_region(data, roi_name='', by='apoe', ax=None, ylim=[0.0005, 0.0010]):
@@ -64,7 +70,6 @@ def plot_region(data, roi_name='', by='apoe', ax=None, ylim=[0.0005, 0.0010]):
     plt.title(roi_name, fontproperties=font)
 
 def boxplot_region(df, groups, by='apo', ax=None):
-    import seaborn as sns
     # Plots the corrected values and fits a line over them
     groups_sub = get_groups(df, groups_names = groups, by=by)
     if ax == None:
@@ -93,49 +98,11 @@ def boxplot_region(df, groups, by='apo', ax=None):
         hdr.append((groups[i1], groups[i2]))
     return pvals, hdr
 
-def lm_plot(data, roi_name, ylim=None):
-    data = pd.DataFrame(data, columns=['apoe','age','gender', roi_name]).rename(columns={roi_name:'roi'})
-    data = data.dropna()
-
-    # correction without ajusting for age
-    adj_model = 'roi ~ 1 + gender'
-    ycorr = pd.DataFrame(correct(data, adj_model), columns=['roi'])
-    del data['roi']
-    df = data.join(ycorr)
-    #print 'Standard deviation after correction for model %s:'%adj_model,\
-    #        np.std(df['roi'])
-    df['subject'] = df.index
-    df = df.sort_values(['apoe', 'subject']).dropna()
-
-    import seaborn as sns
-
-    lm = sns.lmplot(x='age', y='roi',  data=df, size=6.2, aspect=1.35, ci=90, hue='apoe', legend=False,
-        palette={2:'#800000', 1:'#ff8000', 0:'#003366'}, truncate=True, sharex=False,sharey=False)
-        #scatter_kws={'linewidths':1,'edgecolor':'#800000','#ff8000','#003366']})
-    ax = lm.axes
-    if ylim is None:
-        ax[0,0].set_ylim([df['roi'].min(), df['roi'].max()])
-    ax[0,0].set_xlim([50,70])
-    ax[0,0].set_yticklabels(['%.2e'%x for x in ax[0,0].get_yticks()])
-    ax[0,0].tick_params(labelsize=12)
-    ax[0,0].set_ylabel('')
-    ax[0,0].set_xlabel('age', fontsize=15, weight='bold')
-    #ax[0,0].set_yticklabels([]) #['%.2e'%x for x in ax[0,0].get_yticks()])
-    #ax[0,0].set_xticklabels([])
-
-    pal = {2:'#ff9999', 1:'#ffd699', 0:'#99ccff'}
-    pal2 = {2:'#800000',1:'#ff8000',0:'#003366'}
-
-    for i, row in df.iterrows():
-        lm.ax.plot(row['roi'], row['age'], ms=5.5, mew=0.4, marker='o', ls='-', mfc=pal[row['apoe']], mec=pal2[row['apoe']], lw=1)
-    #lm.savefig('/tmp/fig.svg')
-    lm.fig.suptitle(roi_name)
 
 def plot_significance(df, x1, x2, groups):
     # statistical annotation
-    import scipy
 
-    T = scipy.stats.ttest_ind(df[df['group']==groups[x1]]['roi'],
+    T = sc.stats.ttest_ind(df[df['group']==groups[x1]]['roi'],
             df[df['group']==groups[x2]]['roi'])
     print T.pvalue
     y, h, col = df['roi'].mean() + df['roi'].std() + 1e-4, 1e-5, 'k'
@@ -146,19 +113,40 @@ def plot_significance(df, x1, x2, groups):
     plt.text((x1+x2)*.5, y+h, '%.3f'%T.pvalue, **opt)
     return T.pvalue
 
-def lm_plot2(data, roi_name):
-    import seaborn as sns
-    roi_name = data.columns[1]
-    df = pd.DataFrame(data, columns=['apoe','age','centiloids', 'gender', roi_name]).rename(columns={roi_name:'roi'})
+def lm_plot(y, x, data, covariates=['gender', 'age'], hue='apoe', ylim=None,
+        savefig=None):
+
+    # building a new table with only needed variables
+    # y is renamed to roi to avoid potential issues with strange characters
+    roi_name = y
+    adj_model = 'roi ~ 1 + %s'%'+'.join(covariates)
+    log.info('Fit model used for correction: %s'%adj_model)
+    log.info('Dependent variable: %s'%y)
+    variables = [x, y, hue]
+    variables.extend(covariates)
+    df = pd.DataFrame(data, columns=variables).rename(columns={y:'roi'})
     df = df.dropna()
-    lm = sns.lmplot(x='age', y='roi',  data=df, size=6.2, hue='centiloids', aspect=1.35, ci=90, legend=False,
+    y = 'roi'
+
+    # correcting depending variable
+    ycorr = pd.DataFrame(correct(df, adj_model), columns=[y])
+    del df[y]
+    df = df.join(ycorr)
+
+    # plotting
+    lm = sns.lmplot(x=x, y=y,  data=df, size=6.2, hue='apoe', aspect=1.35, ci=90,
 	 truncate=True, sharex=False,sharey=False)
     ax = lm.axes
-    ax[0,0].set_ylim([df['roi'].min(), df['roi'].max()])
-    ax[0,0].set_xlim([50,70])
-    ax[0,0].set_yticklabels(['%.2e'%x for x in ax[0,0].get_yticks()])
+    if ylim is None:
+        ax[0,0].set_ylim([df[y].min(), df[y].max()])
+    else:
+        ax[0,0].set_ylim(ylim)
+    ax[0,0].set_xlim([df[x].min(), df[x].max()])
+    ax[0,0].set_yticklabels(['%.2e'%i for i in ax[0,0].get_yticks()])
     ax[0,0].tick_params(labelsize=12)
     ax[0,0].set_ylabel('')
-    ax[0,0].set_xlabel('age', fontsize=15, weight='bold')
-    lm.ax.plot(df['roi'], df['age'], ms=5.5, mew=0.4, marker='o', ls='-', lw=1)
+    ax[0,0].set_xlabel(x, fontsize=15, weight='bold')
     lm.fig.suptitle(roi_name)
+
+    if not savefig is None:
+        lm.savefig(savefig)
